1d63611011734d618874002aebe597264c306c1c
vinithk32 <vinithk32@gmail.com>
2020-02-19 22:21:14 +0530

    Adding home page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,112 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <head>
+            <meta charset="utf-8"></meta>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <title>GlobalAzureBootcamp2018 : Home</title>
+            <link rel="shortcut icon" type="image/icon" href="assets/images/favicon.ico" />
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"></link>
+            <link href="assets/css/bootstrap.min.css" rel="stylesheet"></link>
+            <link href="assets/css/footer.css" rel="stylesheet"></link>
+
+            <link href="assets/css/slick.css" rel="stylesheet"></link>
+            <link id="switcher" href="assets/css/theme-color/jelly-bean-theme.css" rel="stylesheet"></link>
+            <link href="style.css" rel="stylesheet"></link>
+            <link href="assets/css/chat-bot.css" rel="stylesheet"></link>
+
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet"></link>
+            <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"></link>
+
+
+                <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          </head>
+          <body>
+          <header id="mu-hero" class="" role="banner">
+		<nav class="navbar navbar-fixed-top navbar-default mu-navbar">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+					 aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+					<a class="navbar-brand" href="index.html">Global Azure Bootcamp 2018</a>
+
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav mu-menu navbar-right">
+						<li>
+							<a href="#mu-hero">Home</a>
+						</li>
+						<li>
+							<a href="#mu-about">About Us</a>
+						</li>
+						<li>
+							<a href="#mu-schedule">Schedule</a>
+						</li>
+						<li>
+							<a href="#mu-speakers">Speakers</a>
+						</li>
+						<li>
+							<a href="#mu-venue">Venue</a>
+						</li>
+						<li>
+							<a href="#mu-register">Register</a>
+						</li>
+						<li>
+							<a href="#mu-sponsors">Sponsors</a>
+						</li>
+						<li>
+								<a href="#mu-communities">Communities</a>
+							</li>
+						<li>
+							<a href="#mu-contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div id="particle-js" /*style="position:absolute;height:100%;width:100%;z-index:10;"*/>
+
+		</div>
+		<div class="mu-hero-overlay">
+			<div class="container">
+				<div class="mu-hero-area">
+
+					<div class="mu-hero-featured-area">
+						<div class="mu-logo-area">
+							<a class="mu-logo" href="#"><img class="" src="assets/images/logo-2018-250x222.png" alt="Men Speaker"></img></a>
+						</div>
+
+						<div class="mu-hero-featured-content">
+
+							<h1 /*style="font-size:75px;"*/><b>Global Azure Bootcamp 2018</b></h1>
+							<h2>Become a better professional with real-world practical insights into current technologies, and connect with like-minded
+								people!
+							</h2>
+							<p class="mu-event-date-line">April 21st, 2018 Bengaluru, India</p>
+
+							<div class="mu-event-counter-area">
+								<div id="mu-event-counter">
+
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+          </body>
       </header>
     </div>
   );
